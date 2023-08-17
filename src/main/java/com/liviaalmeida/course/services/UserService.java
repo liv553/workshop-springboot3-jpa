49f17d43f7eed275c485c1b94d26e8d81ac74793
liv553 <livia.balmeida@ufpe.br>
2023-08-16 23:06:41 -0300

    Exception handling - findById

--- src/main/java/com/liviaalmeida/course/services/UserService.java
+++ src/main/java/com/liviaalmeida/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.liviaalmeida.course.entities.User;
 import com.liviaalmeida.course.repositories.UserRepository;
+import com.liviaalmeida.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
